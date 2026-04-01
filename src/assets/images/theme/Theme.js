c0538efd131f78030e12ad53697397f0cf9440c6
Saffan Khan <saffan.khan175@gmail.com>
2026-04-01 17:57:59 +0500

    startup

---
+++ src/assets/images/theme/Theme.js
@@ -0,0 +1,17 @@
+export const COLOURS = {
+
+    grey: '#777',
+    white: '#FFF',
+    black: '#000',
+    red: '#FF0103',
+    green:'#68BE0E',
+    yellow:'#d7e601',
+    light_grey:'#eee',    
+    primary: '#FF7008',
+    light_black: '#6C7278',
+    transparent: 'rgba(0,0,0,0)',
+    green_disbale:'rgba(52, 168, 83, 1)',
+    white_transparent: 'rgba(0, 0, 0, 0.08)',
+    disablecolor: '#808080' //disable color
+
+} 
